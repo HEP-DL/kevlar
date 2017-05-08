9bfce3fa765d0cbff8a4c8a6de94259f5f2a29cf
Kevin Wierman <kwierman@gmail.com>
2017-05-08 13:19:04 -0500

    Bugfix

--- kevlar/Utilities/Version.hh
+++ kevlar/Utilities/Version.hh
@@ -7,6 +7,6 @@
 #define KEVLAR_VERSION "v04_01_00"
   
 //Compile_RUNTIME_ASSERT(H5_HAVE_FILTER_DEFLATE, PleaseCompileHDF5WithZLibSupport);
-static_assert(H5_HAVE_FILTER_DEFLATE, "Please Compile HDF5 With ZLib Support")
+static_assert(H5_HAVE_FILTER_DEFLATE, "Please Compile HDF5 With ZLib Support");
 
 #endif
